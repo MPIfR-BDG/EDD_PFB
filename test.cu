9bee760485e90371484bec92d2d9053ec28afc4a
Tobias Winchen <tobias.winchen@rwth-aachen.de>
2019-09-18 10:37:16 +0000

    Added test for pack and strip

--- test.cu
+++ test.cu
@@ -31,11 +31,74 @@ TEST(FIRFilterKernel, TestSum)
 
       EXPECT_EQ(output[*minmax.first], nTaps) << "FFTSize: " << fftSize << ", " << "NTaps: " << nTaps ;
       EXPECT_EQ(output[*minmax.second], nTaps) << "FFTSize: " << fftSize << ", " << "NTaps: " << nTaps ;
-
-
     }
   }
 }
+
+TEST(OutputPackAndStrip, 8bit)
+{
+  thrust::device_vector<cufftComplex> ppfData;
+	thrust::device_vector<uint8_t> packed;
+  size_t nSpectra = 128;
+  size_t fftSize = 64;
+  ppfData.resize(nSpectra * (fftSize / 2 + 1));
+  packed.resize(nSpectra * (fftSize));
+
+  for (size_t i =0; i < nSpectra; i++)
+  {
+    for (size_t j = 0; j < fftSize / 2 + 1; j++)
+    {
+      float v = 2 * i * fftSize / 2 + 2 * (j - 1);
+      ppfData[i * (fftSize / 2 + 1) + j] = make_cuFloatComplex(v, v + 1);
+    }
+    ppfData[i * (fftSize / 2 + 1)] = make_cuFloatComplex(-1., -2);;
+  }
+
+  packNbitAndStripDC<8><<<128, 1024>>>((float*) thrust::raw_pointer_cast(ppfData.data()),
+              (uint32_t*) thrust::raw_pointer_cast(packed.data()), fftSize, nSpectra, -128, 127);
+
+  for (size_t i =0; i < nSpectra * fftSize; i++)
+  {
+    if (i < 127)
+      EXPECT_EQ(i, packed[i] - 128);
+    else
+      EXPECT_EQ(packed[i], 255);
+  }
+
+
+}
+
+TEST(OutputPackAndStrip, 32bit)
+{
+	thrust::device_vector<cufftComplex> ppfData;
+	thrust::device_vector<uint32_t> packed;
+  size_t nSpectra = 128;
+  size_t fftSize = 64;
+  ppfData.resize(nSpectra * (fftSize / 2 + 1));
+  packed.resize(nSpectra * (fftSize));
+
+  for (size_t i =0; i < nSpectra; i++)
+  {
+    for (size_t j = 0; j < fftSize / 2 + 1; j++)
+    {
+      float v = i * fftSize / 2 + j - 1;
+      ppfData[i * (fftSize / 2 + 1) + j] = make_cuFloatComplex(v, v + .5);
+    }
+    ppfData[i * (fftSize / 2 + 1)] = make_cuFloatComplex(-1., -2);;
+  }
+  stripDCChannel<<<128, 1024>>>((float*) thrust::raw_pointer_cast(ppfData.data()),
+              (float*) thrust::raw_pointer_cast(packed.data()), fftSize, nSpectra);
+
+  for (size_t i = 0; i < nSpectra * fftSize; i += 2)
+  {
+    uint32_t data_p_r = packed[i];
+    uint32_t data_p_i = packed[i+1];
+
+    EXPECT_FLOAT_EQ(float (i / 2), *(float*)(&data_p_r));
+    EXPECT_FLOAT_EQ(float (i / 2) + .5, *(float*)(&data_p_i));
+  }
+}
+
 
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
